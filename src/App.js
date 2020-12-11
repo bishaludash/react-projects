f5cd117bfc6a8e191cd3485deb0a3d6b8882b86a
Bishal Udash <bishal.udash@mobwizards.com>
2020-12-11 17:11:16 +0545

    first commit clean unwanted datas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Navbar from './components/layouts/Navbar'
+import UserItem from './components/users/UserItem'
 import './App.css';
 
 function App() {
+	const name = "Bishal Udash";
+	const foo = () => "bars";
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+    	<Navbar />
+      <UserItem usertitle="chief" />
+
+      <h1>Hello {name}</h1>
+      <p>{foo()}</p>
     </div>
   );
 }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const UserItem = (props) => {
+	return (
+		<div>
+			useritem {props.usertitle}
+		</div>
+	)
+}
+
+export default UserItem
