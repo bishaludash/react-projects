bd9b59f1222c9fc56f04908a5df15e37e3eef583
Bishal Udash <bishal.udash@mobwizards.com>
2020-12-29 17:25:27 +0545

    use navlinks is sub links

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ import Login from "./components/auth/Login";
 const App = () => {
   return (
     <Router>
-      This is app.
       <nav>
         <NavLink to='/home'  activeClassName="activeLink" className="nav-links" >Home</NavLink>
         <NavLink to='/login' activeClassName="activeLink" className="nav-links"   >Login</NavLink>
-        <NavLink to='/test' activeClassName="activeLink" className="nav-links">Login</NavLink>
       </nav>
+      
       <Switch>
         <Route path="/home" component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/test" render={()=>{return "test"}} />
         <Redirect to='/home'></Redirect>
       </Switch>
     </Router>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Switch, Route, useRouteMatch ,Link} from "react-router-dom";
+import { Switch, Route, useRouteMatch ,NavLink, Redirect} from "react-router-dom";
 import Detail from './Detail';
 import Work from './Work';
 
 const Home = () => {
   const match = useRouteMatch();
+  console.log(match);
 
   return (
     <div>
     <ul>
       <li>
-      <Link to='/home/details'>Details</Link>
+      <NavLink activeClassName="activeLink"to={`${match.url}/details`}>Details</NavLink>
       </li>
       <li>
-      <Link to='/home/works'>Work</Link>
+      <NavLink activeClassName="activeLink" to={`${match.url}/works`}>Work</NavLink>
       </li>
     </ul>
 
@@ -21,6 +22,7 @@ const Home = () => {
         <Route path='/home' exact render={()=>{return "this is home"}} />
         <Route path='/home/details' exact component={Detail} />
         <Route path='/home/works' exact component={Work} />
+        <Redirect to={()=>{return "not found"}}></Redirect>
       </Switch>
     </div>
   )
